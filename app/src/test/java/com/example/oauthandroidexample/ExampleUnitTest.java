d3cf24c74494f9ec711445ed9e562a97c7aca01f
krsnmr <krasnoemore@hotmail.com>
2018-08-29 16:47:42 +0300

    тест коммит

--- app/src/test/java/com/example/oauthandroidexample/ExampleUnitTest.java
+++ app/src/test/java/com/example/oauthandroidexample/ExampleUnitTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(5, 3 + 2);
+        assertEquals(77, 43 + 34);
     }
 
 
